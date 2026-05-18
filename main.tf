3995bafd78b09574555ede8cdbe2b6b1371bbb81
JacobSidhu <148295294+JacobSidhu@users.noreply.github.com>
2026-05-18 00:20:57 +0000

    Add aws ami and aws availability zones data.

--- main.tf
+++ main.tf
@@ -6,6 +6,25 @@ locals {
   }
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
