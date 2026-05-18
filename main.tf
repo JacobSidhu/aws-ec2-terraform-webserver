6feae747487be7fd99261a1417ae45b9530ce7d1
JacobSidhu <148295294+JacobSidhu@users.noreply.github.com>
2026-05-18 00:15:30 +0000

    Add locals common tags.

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+locals {
+  common_tags = {
+    project     = "azure-vm-terraform-webserver"
+    environment = "demo"
+    managed_by  = "terraform"
+  }
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
