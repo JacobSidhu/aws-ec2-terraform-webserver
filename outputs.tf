c0f453420cd8d523411fe7aa0746693b7eb5744d
JacobSidhu <148295294+JacobSidhu@users.noreply.github.com>
2026-05-18 03:00:14 +0000

    feat: expose EC2 public IP and web URL outputs.

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,28 @@ output "vpc_cidr_block" {
   description = "CIDR block of the created VPC"
   value       = aws_vpc.main.cidr_block
 }
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
+
+output "public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.web.public_dns
+}
+
+output "web_url" {
+  description = "HTTP URL for the Nginx web server"
+  value       = "http://${aws_instance.web.public_ip}"
+}
+
+output "ssh_command" {
+  description = "SSH command to connect to the EC2 instance"
+  value       = "ssh ubuntu@${aws_instance.web.public_ip}"
+}
