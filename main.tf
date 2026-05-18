62cff6194a53c744d1409204c7e354876c5b9dcf
JacobSidhu <148295294+JacobSidhu@users.noreply.github.com>
2026-05-18 00:25:25 +0000

    Add vpc configurations.

--- main.tf
+++ main.tf
@@ -26,10 +26,11 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-  tags = {
-    Name      = "${var.project_name}-vpc"
-    ManagedBy = "terraform"
-  }
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-vpc"
+  })
 }
