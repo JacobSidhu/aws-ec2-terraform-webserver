d85918c7683ee968fa80da2072dcba1ef1b0c8d8
JacobSidhu <148295294+JacobSidhu@users.noreply.github.com>
2026-05-18 01:09:21 +0000

    Add aws public subnet with public ip mapping ON.

--- main.tf
+++ main.tf
@@ -34,3 +34,14 @@ resource "aws_vpc" "main" {
     Name = "${var.project_name}-vpc"
   })
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-public-subnet"
+  })
+}
